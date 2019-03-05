6377b9f812ed0bf44a4c060e35ba05c7df9faa33
Oscar Munoz <omunozg@emory.edu>
2019-03-05 05:12:49 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,6 +1,6 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
-def meal choice(value = "meat")
+def meal choice(c = "meat")
   return value
 end
